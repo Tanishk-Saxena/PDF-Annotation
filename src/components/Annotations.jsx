0ea83275026b1ae9030cbe95f5a0947646669661
Tanishk Saxena <tanishksaxena686@outlook.com>
2023-01-03 21:12:09 +0530

    making notations and storing in local storage

---
+++ src/components/Annotations.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Stage, Layer, Rect } from "react-konva";
+
+const Annotations = ({mode, modeObject, annotations, setAnnotations}) => {
+  const [newAnnotation, setNewAnnotation] = useState([]);
+
+  const handleMouseDown = event => {
+    if (newAnnotation.length === 0 && mode) {
+      const { x, y } = event.target.getStage().getPointerPosition();
+      setNewAnnotation([{ x, y, width: 0, height: 0, key: "0", ...modeObject }]);
+    }
+  };
+
+  const handleMouseUp = event => {
+    if (newAnnotation.length === 1 && mode) {
+      const sx = newAnnotation[0].x;
+      const sy = newAnnotation[0].y;
+      const { x, y } = event.target.getStage().getPointerPosition();
+      const annotationToAdd = {
+        x: sx,
+        y: sy,
+        width: x - sx,
+        height: y - sy,
+        key: annotations.length + 1,
+        ...modeObject
+      };
+      if(annotationToAdd.width && annotationToAdd.height){
+        let newAnnotationArray = annotations.concat(annotationToAdd);
+        setAnnotations(newAnnotationArray);
+      }
+      setNewAnnotation([]);
+    }
+  };
+
+  const handleMouseMove = event => {
+    if (newAnnotation.length === 1 && mode) {
+      const sx = newAnnotation[0].x;
+      const sy = newAnnotation[0].y;
+      const { x, y } = event.target.getStage().getPointerPosition();
+      setNewAnnotation([
+        {
+          x: sx,
+          y: sy,
+          width: x - sx,
+          height: y - sy,
+          key: "0",
+          ...modeObject
+        }
+      ]);
+    }
+  };
+
+  const annotationsToDraw = [...annotations, ...newAnnotation];
+  console.log(annotationsToDraw);
+  
+  let initialWidth = 700;
+  let initialHeight = 700;
+  const [width, setWidth] = useState(initialWidth);
+  const [height, setHeight] = useState(initialHeight);
+  
+  // function fitStageIntoParentContainer() {
+  //   var container = document.querySelector('#stage-parent');
+    
+  //   // now we need to fit stage into parent container
+  //   var containerWidth = container.offsetWidth;
+    
+  //   // but we also make the full scene visible
+  //   // so we need to scale all objects on canvas
+  //   var scale = containerWidth / initialWidth;
+    
+  //   width(initialWidth * scale);
+  //   height(initialHeight * scale);
+  //   // scale({ x: scale, y: scale });
+  // }
+  
+  // // fitStageIntoParentContainer();
+  // // adapt the stage on any window resize
+  // window.addEventListener('resize', fitStageIntoParentContainer);
+
+  return (
+    <Stage
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      width={width}
+      height={height}
+    >
+      <Layer>
+        {annotationsToDraw && annotationsToDraw.map(value => {
+          return (
+            <Rect
+              x={value.x}
+              y={value.y}
+              width={value.width}
+              height={value.height}
+              fill={value.color}
+              stroke={value.strokeColor}
+            />
+          );
+        })}
+      </Layer>
+    </Stage>
+  );
+};
+
+export default Annotations;
--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -1,9 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Annotations from '../components/Annotations';
 
 const Document = ({doc}) => {
 
     const url = `https://arxiv.org/pdf/2212.${doc}.pdf`;
     const embedded_url = `https://docs.google.com/viewer?url=${url}&embedded=true`;
+    const titleModeObject = {color: 'rgba(231, 103, 57, 0.428)', strokeColor: 'rgba(255, 68, 0, 0.728)', class: 'title'};
+    const authorModeObject = {color: 'rgba(49, 166, 49, 0.41)', strokeColor: 'rgba(0, 128, 0, 0.71)', class: 'author'};
+    const [mode, setMode] = useState(null);
+    const [modeObject, setModeObject] = useState({});
+    const localAnnotationsArray = JSON.parse(localStorage.getItem(doc));
+    const [annotations, setAnnotations] = useState(localAnnotationsArray?localAnnotationsArray:[]);
 
     useEffect(() => {
       const fetchPDF = async (url) => {
@@ -11,19 +18,32 @@ const Document = ({doc}) => {
         console.log(res);
       }
       fetchPDF(url);
-    }, [])
-    
+    }, []);
 
+    useEffect(()=>{
+      if(mode === 'titleMode'){
+        setModeObject(titleModeObject);
+      }else if(mode === 'authorMode'){
+        setModeObject(authorModeObject);
+      }else{
+        setModeObject({});
+      }
+    }, [mode]);
+
+    useEffect(()=>{
+      localStorage.setItem(doc, JSON.stringify(annotations));
+    }, [annotations]);
+    
   return (
     <div className="container">
       <div className="left heading">
         <h3>Labels</h3>
         <hr />
-        <button className='title heading-btn'>Title</button>
-        <button className='author heading-btn'>Author</button>
+        <button id="title-btn" className='title heading-btn' onClick={()=>{let titleBtn = document.getElementById('title-btn'); let authorBtn = document.getElementById('author-btn'); if(mode === 'titleMode'){setMode(null); titleBtn.classList.remove('title-active');}else{setMode('titleMode'); titleBtn.classList.add('title-active'); authorBtn.classList.remove('author-active');}}}>Title</button>
+        <button id="author-btn" className='author heading-btn' onClick={()=>{let titleBtn = document.getElementById('title-btn'); let authorBtn = document.getElementById('author-btn'); if(mode === 'authorMode'){setMode(null); authorBtn.classList.remove('author-active');}else{setMode('authorMode'); authorBtn.classList.add('author-active'); titleBtn.classList.remove('title-active');}}}>Author</button>
       </div>
       <div className="left content">
-        <h3>Boxes</h3>
+        <h3>Annotations</h3>
         <hr />
         <p>Hello There!</p>
         <p>Hello There!</p>
@@ -40,7 +60,10 @@ const Document = ({doc}) => {
         <p>Hello There!</p>
       </div>
       <div className="right pdf">
-        <iframe src={embedded_url} frameborder="0" style={{height: "94vh", width: "100%"}}></iframe>
+        <div className="annotations" style={{height: "94vh", width: "100%"}}>
+          <Annotations mode={mode} modeObject={modeObject} annotations={annotations} setAnnotations={setAnnotations}/>
+        </div>
+        <iframe src={embedded_url} frameBorder="0" style={{height: "100%", width: "100%"}}></iframe>
       </div>
     </div>
   )
